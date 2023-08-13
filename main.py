48d225d71831ea453c9a766b52409f12a393e466
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-08-12 22:05:27 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+import openai
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+class ImageVariationGenerator:
+    def __init__(self, model_name):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.image_variations = []
+
+    def generate_image_variations(self, image_file):
+        response = {'data': [{'url': f'https://example.com/{image_file}'}]}
+        self.image_variations.append(response['data'][0]['url'])
+
+    def get_image_variations(self):
+        return self.image_variations
+
+class ChatBot:
+    def __init__(self, image_var_generator):
+        self.image_var_generator = image_var_generator
+        self.chat_history = None
+
+    def simulate_conversation(self, conversation):
+        response = {'choices': [{'message': {'content': 'Response from ChatBot'}}]}
+        return response['choices'][0]['message']['content']
+
+    def generate_response(self, user_input):
+        new_user_input_ids = self.image_var_generator.tokenizer.encode(user_input + self.image_var_generator.tokenizer.eos_token, return_tensors='pt')
+        bot_input_ids = torch.cat([self.chat_history, new_user_input_ids], dim=-1) if self.chat_history is not None else new_user_input_ids
+        chat_history_ids = self.image_var_generator.model.generate(bot_input_ids, max_length=1000,
+                                                                   pad_token_id=self.image_var_generator.tokenizer.eos_token_id,
+                                                                   do_sample=True, num_return_sequences=1)
+        self.chat_history = bot_input_ids
+        return self.image_var_generator.tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0],
+                                                         skip_special_tokens=True)
+
+    def engage_with_users(self, num_lines):
+        for _ in range(num_lines):
+            user_input = input(">> User: ")
+            response = self.generate_response(user_input)
+            print("DialoGPT: {}".format(response))
+
+class ImageTextApp:
+    def __init__(self):
+        self.image_var_generator = ImageVariationGenerator("microsoft/DialoGPT-medium")
+        self.chat_bot = ChatBot(self.image_var_generator)
+
+    def run(self):
+        self.image_var_generator.generate_image_variations("corgi_and_cat_paw.png")
+        self.chat_bot.engage_with_users(5)
+
+if __name__ == '__main__':
+    app = ImageTextApp()
+    app.run()
